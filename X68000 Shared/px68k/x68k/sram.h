a2d48f83cbb5624bc46e9a912ef8e490fe942fd7
Yos Awed <4214168+YosAwed@users.noreply.github.com>
2023-10-07 15:36:50 +0900

    CGROM and IPLROM array added

--- X68000 Shared/px68k/x68k/sram.h
+++ X68000 Shared/px68k/x68k/sram.h
@@ -5,6 +5,9 @@
 
 extern	BYTE	SRAM[0x4000];
 
+extern unsigned char CGROM_DAT[0xc0000]; // added by Awed 2023/10/7
+extern unsigned char IPLROM_DAT[0x20000]; // added by Awed 2023/10/7
+
 void SRAM_Init(void);
 void SRAM_Cleanup(void);
 void SRAM_VirusCheck(void);
